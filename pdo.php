f17ade1ed738b6a120752d6232a0ebeffdbac894
guncebektas <guncebektas@gmail.com>
2015-04-30 18:01:55 +0300

    improvements

--- pdo.php
+++ pdo.php
@@ -8,35 +8,12 @@
  * 
  * otherwise you will only create query string!
  * 
- * insert_id, find, columns, insert methods will be exacuted directly 
+ * insert_id, find, columns, insert etc... will be exacuted directly 
  * 
  * Examples:
+ * PS 1: You can put array into values like values($_POST) if columns match with the index of array
  * 
- * 1. THIS WILL SELECT ALL ROWS IN SLIDES TABLE
-   select('slides')->results();	
-
- * 
- * 
- * 2. INSERT ARRAY INTO SLIDES TABLE 
-
-   insert('slides')->values(array('slide_img'=>$_POST['slide_img'], 
-									  'slide_title'=>$_POST['slide_title'],
-									  'slide_text'=>$_POST['slide_text'],
-									  'slide_href'=>$_POST['slide_href']));
-
- * 
- * 
- * 3. UPDATE SLIDES TABLE 
-  
-	update('slides')->values(array('slide_img'=>$_POST['slide_img'], 
-									  'slide_title'=>$_POST['slide_title'],
-									  'slide_text'=>$_POST['slide_text'],
-									  'slide_href'=>$_POST['slide_href']))->where('slide_id = 1');
-
- * 
- * PS 1: you can put array into values like values($_POST) if columns match with the index of array
- * 
- * PS 2: use security function in where clause to block SQL injection like 
+ * PS 2: Use security function in where clause to block SQL injection like 
  * ->where('slide_id = '.security($_GET['slide_id']));
  */
  
@@ -49,6 +26,8 @@ $db = array(
   'pass' => '',
   'charset' => 'charset=utf8',
 );
+
+require_once 'core/lib/classes/Pdo.php';
 $pdo = new _pdo($db);
 
 class _pdo extends PDO
@@ -510,19 +489,35 @@ class _pdo_statement extends PDOStatement
     }
 }
 
-// Use these functions instead of $pdo->select() in functions and classes
+/* Use these functions instead of $pdo->select() usage.
+ * 
+ */
+
+/* select('slides')->results();	
+ * 
+ * or
+ * 
+ * select('slides')->where('slide_id = 2')->limit(1)->result();	
+ * 
+ */
 function select($table)
 {
     global $pdo;
 
     return $pdo->select($table);
 }
+/* find('slides',3);	
+ * 
+ */
 function find($table, $id)
 {
     global $pdo;
 
     return $pdo->find($table, $id);
 }
+/* insert('slides')->values(array('slide_img'=>$_POST['slide_img'], 'slide_title'=>$_POST['slide_title'],'slide_text'=>$_POST['slide_text'],'slide_href'=>$_POST['slide_href']));
+ * 
+ */
 function insert($table)
 {
     global $pdo;
@@ -535,15 +530,123 @@ function replace($table)
 
     return $pdo->replace($table);
 }
+/* update('slides')->values(array('slide_img'=>$_POST['slide_img'], 'slide_href'=>$_POST['slide_href']))->where('slide_id = 1');
+ * 
+ */
 function update($table)
 {
     global $pdo;
 
     return $pdo->update($table);
 }
+/* delete('slides')->where('slide_id = 2');
+ * 
+ * or
+ * 
+ * delete('slides',2);
+ * 
+ */
 function delete($table, $key = '')
 {
     global $pdo;
 
     return $pdo->delete($table, $key);
+}
+/* last_id();
+ * 
+ */
+function last_id()
+{
+	global $pdo;
+	
+	return $pdo->insert_id();
+}
+
+
+
+
+
+
+
+
+
+
+// Main security function to check strings
+function security($input)
+{
+    // Clear not allowed chars
+    $input = preg_replace('/([\x00-\x08][\x0b-\x0c][\x0e-\x20])/', '', $input);
+
+    // Search for these
+    $search = 'abcdefghijklmnopqrstuvwxyz';
+    $search .= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $search .= '1234567890!@#$%^&*()';
+    $search .= '~`";:?+/={}[]-_|\'\\';
+
+    // Clear not allowed chars again
+    for ($i = 0; $i < strlen($search); $i++) {
+        $input = preg_replace('/(&#[x|X]0{0,8}'.dechex(ord($search[$i])).';?)/i', $search[$i], $input);
+        $input = preg_replace('/(&#0{0,8}'.ord($search[$i]).';?)/', $search[$i], $input);
+    }
+
+    // Remove java, flash etc..
+    $ra1 = array('javascript', 'vbscript', 'expression', 'applet', 'meta', 'xml', 'blink', 'link', 'style', 'script', 'embed', 'object', 'iframe', 'frame', 'frameset', 'ilayer', 'layer', 'bgsound', 'title', 'base');
+    $ra2 = array('onabort', 'onactivate', 'onafterprint', 'onafterupdate', 'onbeforeactivate', 'onbeforecopy', 'onbeforecut', 'onbeforedeactivate', 'onbeforeeditfocus', 'onbeforepaste', 'onbeforeprint', 'onbeforeunload', 'onbeforeupdate', 'onblur', 'onbounce', 'oncellchange', 'onchange', 'onclick', 'oncontextmenu', 'oncontrolselect', 'oncopy', 'oncut', 'ondataavailable', 'ondatasetchanged', 'ondatasetcomplete', 'ondblclick', 'ondeactivate', 'ondrag', 'ondragend', 'ondragenter', 'ondragleave', 'ondragover', 'ondragstart', 'ondrop', 'onerror', 'onerrorupdate', 'onfilterchange', 'onfinish', 'onfocus', 'onfocusin', 'onfocusout', 'onhelp', 'onkeydown', 'onkeypress', 'onkeyup', 'onlayoutcomplete', 'onload', 'onlosecapture', 'onmousedown', 'onmouseenter', 'onmouseleave', 'onmousemove', 'onmouseout', 'onmouseover', 'onmouseup', 'onmousewheel', 'onmove', 'onmoveend', 'onmovestart', 'onpaste', 'onpropertychange', 'onreadystatechange', 'onreset', 'onresize', 'onresizeend', 'onresizestart', 'onrowenter', 'onrowexit', 'onrowsdelete', 'onrowsinserted', 'onscroll', 'onselect', 'onselectionchange', 'onselectstart', 'onstart', 'onstop', 'onsubmit', 'onunload');
+
+    // Merge arrays
+    $ra = array_merge($ra1, $ra2);
+
+    // Remove possible threats which are defined above
+    $find = true;
+    while ($find == true) {
+        $first = $input;
+        for ($i = 0; $i < sizeof($ra); $i++) {
+            $action = '/';
+            for ($j = 0; $j < strlen($ra[$i]); $j++) {
+                if ($j > 0) {
+                    $action .= '(';
+                    $action .= '(&#[x|X]0{0,8}([9][a][b]);?)?';
+                    $action .= '|(&#0{0,8}([9][10][13]);?)?';
+                    $action .= ')?';
+                }
+                $action .= $ra[$i][$j];
+            }
+
+            $action .= '/i';
+            $change = substr($ra[$i], 0, 2).'<x>'.substr($ra[$i], 2);
+            $input = preg_replace($action, $change, $input);
+
+            if ($first == $input) {
+                $find = false;
+            }
+        }
+    }
+
+    // Allowed tags
+    $result = strip_tags($input, '<p><strong><em><b><i><ul><li><pre><hr><blockquote><span>');
+
+    // Change special chars to their html version
+    $result = htmlspecialchars($result);
+
+    // \n to <br>
+    $result = str_replace("\n", '<br />', $result);
+
+    // Add slash
+    $result = addslashes($result);
+
+    return $result;
+}
+
+// Clear unnecessary chars
+function clean($input)
+{
+    $input = str_replace("\'", "'", $input);
+    $input = str_replace('\\\\', '\\', $input);
+    $input = str_replace('<br />', "\n", $input);
+    $input = str_replace('&amp;', '&', $input);
+    $input = str_replace('&quot;', '"', $input);
+    $input = str_replace('<', '&lt;', $input);
+    $input = str_replace('>', '&gt;', $input);
+
+    return $input;
 }
